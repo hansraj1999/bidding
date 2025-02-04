5517a7b4b2ec77fc5a5969e4e1e0133f83a21260
Hansraj Deghun <hansrajdeghun@gofynd.com>
2025-02-05 03:28:18 +0530

    initial commit

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,6 @@
 import uvicorn
 import os
-print(os.getenv("mongo_con"), "ofnonfonfon")
+print(os.getenv("mongodb://fynd:fynd1234@docdb-2025-02-04-21-36-32.cluster-c302wsi4sxgz.ap-south-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"), "ofnonfonfon")
 
 if __name__ == "__main__":
     uvicorn.run(
